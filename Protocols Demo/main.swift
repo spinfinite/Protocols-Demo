88289a252c4ccd6f28239ade1be0e7dd42b6ca84
Sergio <spinfinite07@gmail.com>
2023-08-07 11:19:11 -0600

    Added protocol functionality that a class and a struct can use.

--- Protocols Demo/main.swift
+++ Protocols Demo/main.swift
@@ -1,11 +1,64 @@
-//
-//  main.swift
-//  Protocols Demo
-//
-//  Created by Dad's 6,1 on 8/7/23.
-//
+protocol CanFly {
+    func fly()
+}
 
-import Foundation
 
-print("Hello, World!")
+class Bird {
+    
+    var isFemale = true
+    
+    
+    func layEgg() {
+        
+        if isFemale {
+            print("The bird makes a new bird in a shell.")
+        }
+        
+    }
+    
+}
+
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+    
+    
+    func soar() {
+        print("The eagle glides in the air using air currents.")
+    }
+}
+
+class Penguin: Bird {
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+struct Airplane: CanFly {
+    func fly(){
+        print("The airplane uses its engine to lift off into the air.")
+    }
+}
+
+let myEagle = Eagle()
+//myEagle.fly()
+//myEagle.layEgg()
+//myEagle.soar()
+
+let myPenguin = Penguin()
+//myPenguin.layEgg()
+//myPenguin.swim()
+//myPenguin.fly()
+
+let myPlane = Airplane()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myPlane)
 
